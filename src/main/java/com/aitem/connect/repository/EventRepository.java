d5efc321972ec64b2a190ed1e3cf9551c74bea7f
erik <nkhangkci@gmail.com>
2021-01-08 05:31:29 +0800

    Test

--- src/main/java/com/aitem/connect/repository/EventRepository.java
+++ src/main/java/com/aitem/connect/repository/EventRepository.java
@@ -1,10 +1,9 @@
 package com.aitem.connect.repository;
 
 import com.aitem.connect.model.EventModel;
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.data.repository.query.Param;
-import org.springframework.data.repository.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 
-public interface EventRepository extends Repository<EventModel, String> {
+public interface EventRepository extends JpaRepository<EventModel, String> {
 }
