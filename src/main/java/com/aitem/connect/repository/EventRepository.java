aff7f6f3d76a83c0a632af7d9cb7802d1f0e0eec
erik <nkhangkci@gmail.com>
2021-01-08 06:51:08 +0800

    Test

--- src/main/java/com/aitem/connect/repository/EventRepository.java
+++ src/main/java/com/aitem/connect/repository/EventRepository.java
@@ -3,7 +3,7 @@ package com.aitem.connect.repository;
 import com.aitem.connect.model.EventModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
-
+import java.util.List;
 
 public interface EventRepository extends JpaRepository<EventModel, String> {
 	List<EventModel> findByCreatorId(@Param("createdBy") String createdBy);
