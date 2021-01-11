e884cc133a9d90f72240dca32dec1b2537c26a90
erik <nkhangkci@gmail.com>
2021-01-12 04:40:28 +0800

    Update

--- src/main/java/com/aitem/connect/service/implementition/CartService.java
+++ src/main/java/com/aitem/connect/service/implementition/CartService.java
@@ -128,10 +128,7 @@ public class CartService implements Cart {
     }
 
     private String getTotal(List<ItemResponse> items) {
-        //return String.valueOf(items.stream().map(item ->Long.valueOf(item.getPrice()) * item.getQuantity()).mapToLong(Long::longValue).sum());
-
-        Double value = items.stream().map(item -> Double.valueOf(item.getPrice()) * item.getQuantity()).mapToDouble(num -> num).sum();
-        return String.valueOf(value);
+        return String.valueOf(items.stream().map(item -> Double.valueOf(item.getPrice()) * item.getQuantity()).mapToDouble(num -> num).sum());
     }
 
     private ItemResponse getItemResponse(OrderDetailsModel orderDetailsModel) {
