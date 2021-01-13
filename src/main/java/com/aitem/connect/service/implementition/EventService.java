ade379b2eb73464e22cb584d722e4a0f6054078a
erik <nkhangkci@gmail.com>
2021-01-13 08:12:35 +0800

    Update

--- src/main/java/com/aitem/connect/service/implementition/EventService.java
+++ src/main/java/com/aitem/connect/service/implementition/EventService.java
@@ -14,6 +14,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
+import java.text.SimpleDateFormat;
 
 @Service
 public class EventService implements Event {
